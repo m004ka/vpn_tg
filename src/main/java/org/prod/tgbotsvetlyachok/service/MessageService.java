4a19c8240042e82336b3b59976f272d8c0741c94
m004ka <ysupov959@gmail.com>
2025-03-23 04:14:52 +0300

    Сервис сообщений

---
+++ src/main/java/org/prod/tgbotsvetlyachok/service/MessageService.java
@@ -0,0 +1,31 @@
+package org.prod.tgbotsvetlyachok.service;
+
+import lombok.RequiredArgsConstructor;
+import org.prod.tgbotsvetlyachok.bot.TelegramBot;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MessageService {
+
+    private final TelegramBot telegramBot;
+
+    private final UpdateService updateService;
+
+    private final KeyBoardService keyBoard;
+    private void PaymentOptions(Update update){
+        EditMessageText message = new EditMessageText();
+        Long id = update.getCallbackQuery().getMessage().getChatId();
+        int messId = update.getCallbackQuery().getMessage().getMessageId();
+        message.setChatId(id);
+        message.setMessageId(messId);
+        message.setParseMode("HTML");
+        message.setText("<b>Продли дни доступа выгодно</b>\uD83D\uDC47");
+        message.setReplyMarkup(keyBoard.getBuyMarkup());
+        telegramBot.sendMessage(message);
+    }
+}
