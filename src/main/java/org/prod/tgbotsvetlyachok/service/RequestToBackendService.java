f0d3c2601cbba7b1aaa6c9ab98ad5c72aaf60814
m004ka <ysupov959@gmail.com>
2025-03-23 04:15:34 +0300

    Реквест сервис

--- src/main/java/org/prod/tgbotsvetlyachok/service/RequestToBackendService.java
+++ src/main/java/org/prod/tgbotsvetlyachok/service/RequestToBackendService.java
@@ -1,7 +1,15 @@
 package org.prod.tgbotsvetlyachok.service;
 
+import org.prod.tgbotsvetlyachok.model.PaymentDTO;
+import org.prod.tgbotsvetlyachok.model.TelegramUser;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public interface RequestToBackendService {
     public boolean checkUser(Update update);
+
+    public boolean createUser(Update update);
+
+    public TelegramUser takeUserById(Long id);
+
+    public String getPaymentUrl(PaymentDTO paymentDTO);
 }
--- src/main/java/org/prod/tgbotsvetlyachok/service/RequestToBackendServiceImpl.java
+++ src/main/java/org/prod/tgbotsvetlyachok/service/RequestToBackendServiceImpl.java
@@ -1,6 +1,8 @@
 package org.prod.tgbotsvetlyachok.service;
 
 import lombok.RequiredArgsConstructor;
+import org.prod.tgbotsvetlyachok.model.PaymentDTO;
+import org.prod.tgbotsvetlyachok.model.TelegramUser;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
@@ -13,4 +15,21 @@ public class RequestToBackendServiceImpl implements RequestToBackendService{
     public boolean checkUser(Update update) {
         return false;
     }
+
+    @Override
+    public boolean createUser(Update update) {
+        return false;
+    }
+
+    @Override
+    public TelegramUser takeUserById(Long id) {
+        return new  TelegramUser();
+    }
+
+    @Override
+    public String getPaymentUrl(PaymentDTO paymentDTO) {
+        return null;
+    }
+
+
 }
