882eb43d7979ed7fa04cf5daf6c704375dd5b1e9
m004ka <ysupov959@gmail.com>
2025-03-23 04:16:56 +0300

    Callback и Update сервисы

---
+++ src/main/java/org/prod/tgbotsvetlyachok/service/TgCallbackService.java
@@ -0,0 +1,9 @@
+package org.prod.tgbotsvetlyachok.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TgCallbackService {
+}
--- src/main/java/org/prod/tgbotsvetlyachok/service/UpdateService.java
+++ src/main/java/org/prod/tgbotsvetlyachok/service/UpdateService.java
@@ -1,14 +1,23 @@
 package org.prod.tgbotsvetlyachok.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.prod.tgbotsvetlyachok.bot.TelegramBot;
+import org.prod.tgbotsvetlyachok.model.PaymentDTO;
+import org.prod.tgbotsvetlyachok.model.TelegramUser;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UpdateService {
 
     private final RequestToBackendService request;
+
+    private final KeyBoardService keyBoardService;
+
+    private final TelegramBot telegramBot;
     public void updateOption(Update update){
         //todo Состояния чата
         if (update.hasMessage() && update.getMessage().hasText()) {
@@ -22,6 +31,12 @@ public class UpdateService {
                     }
                     //todo Получение юзера добавление его на бэк или же проверка на существование
                     break;
+                case "/pay":
+
+                    break;
+
+                default:
+                    break;
 
             }
         } else if (update.hasCallbackQuery()){
@@ -33,10 +48,57 @@ public class UpdateService {
     }
 
     private void startCommandNewUser(Update update){
+        request.createUser(update);
+        TelegramUser tgUser = request.takeUserById(userGetId(update));
+
 
     }
 
     private void startCommand(Update update){
+        TelegramUser tgUser = request.takeUserById(userGetId(update));
 
+    }
+
+    private String getUrl(Update update, Double value){
+       PaymentDTO payDTO = PaymentDTO.builder()
+               .telegramUserId(userGetId(update))
+               .email("мимопока")
+               .receipt(false)
+               .value(value)
+               .build();
+
+       return request.getPaymentUrl(payDTO);
+    }
+
+
+
+
+
+    private Long userGetId(Update update){
+        Long id = null;
+
+        if (update.getCallbackQuery() != null) {
+            try {
+                id = update.getCallbackQuery().getMessage().getChatId();
+                if (id != null) {
+                    return id;
+                }
+            } catch (RuntimeException e) {
+                log.error("Не удалось получить айди через getCallbackQuery: " + e.getMessage());
+            }
+        }
+
+        if (update.getMessage() != null) {
+            try {
+                id = update.getMessage().getChatId();
+                if (id != null) {
+                    return id;
+                }
+            } catch (RuntimeException e) {
+                log.error("Не удалось получить айди через getMessage: " + e.getMessage());
+            }
+        }
+
+        return id;
     }
 }
