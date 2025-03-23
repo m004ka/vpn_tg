b0f7f44da49d2937e430c6fc06a31c6bfb5d89ac
m004ka <ysupov959@gmail.com>
2025-03-23 04:16:18 +0300

    телеграм бот, отправка сообщений

--- src/main/java/org/prod/tgbotsvetlyachok/bot/TelegramBot.java
+++ src/main/java/org/prod/tgbotsvetlyachok/bot/TelegramBot.java
@@ -5,7 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.prod.tgbotsvetlyachok.config.BotConfig;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.methods.send.SendSticker;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Slf4j
 @Component
@@ -21,5 +26,58 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Override
     public String getBotUsername() {
         return botConfig.getBotName();
+    }
+
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(textToSend);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+        }
+
+    }
+
+    public void sendMessage(SendMessage textToSend) {
+        try {
+            execute(textToSend);
+            System.out.println("не Ошибка");
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+            System.out.println("Ошибка");
+        }
+
+    }
+
+
+
+    public void sendMessage(EditMessageText textToSend) {
+        try {
+            execute(textToSend);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+        }
+
+    }
+
+    public void sendMessage(SendPhoto textToSend) {
+        try {
+            execute(textToSend);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+        }
+
+    }
+
+    public void sendMessage(SendSticker textToSend) {
+        try {
+            execute(textToSend);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+        }
+
     }
 }
